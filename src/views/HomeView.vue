81f9e60316cbe13993bcb17f9f917c802c530441
Waqar Shams <97440405+waqarshamstanoli@users.noreply.github.com>
2023-12-01 11:50:57 +0500

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@
       <v-col cols="12" lg="12" md="12" sm="12">
         <div class="text-center mt-10">
           <p
-            class="mt-md-16 text-center font-weight-black openSans mb-0 text-h1"
+            class="mt-md-16 text-center font-weight-black ralewayy mb-0 text-h1"
           >
             How can Zimed® PF help?
           </p>
